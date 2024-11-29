6cad245487d50c27b2a1274fe9a0e5784376f280
ninchester <5041877+ninchester@users.noreply.github.com>
2024-11-29 16:09:15 +0100

    Extract template parts in /partials

---
+++ wp-content/themes/softuni/partials/tabs-section.php
@@ -0,0 +1,139 @@
+<section id="tabs" class="tabs section light-background">
+
+    <div class="container">
+        <div class="row gap-x-lg-4 justify-content-between">
+            <div class="col-lg-4 js-custom-dots">
+                <a href="#" class="service-item link horizontal d-flex active" data-aos="fade-left" data-aos-delay="0">
+                    <div class="service-icon color-1 mb-4">
+                        <i class="bi bi-alarm"></i>
+                    </div>
+                    <!-- /.icon -->
+                    <div class="service-contents">
+                        <h3>Modern and clean design</h3>
+                        <p>
+                            Far far away, behind the word mountains, far from the countries .
+                        </p>
+                    </div>
+                    <!-- /.service-contents-->
+                </a>
+                <!-- /.service -->
+
+                <a href="#" class="service-item link horizontal d-flex" data-aos="fade-left" data-aos-delay="100">
+                    <div class="service-icon color-2 mb-4">
+                        <i class="bi bi-bag-check"></i>
+                    </div>
+                    <!-- /.icon -->
+                    <div class="service-contents">
+                        <h3>Sell Products</h3>
+                        <p>
+                            Far far away, behind the word mountains, far from the countries .
+                        </p>
+                    </div>
+                    <!-- /.service-contents-->
+                </a>
+                <!-- /.service -->
+
+                <a href="#" class="service-item link horizontal d-flex" data-aos="fade-left" data-aos-delay="200">
+                    <div class="service-icon color-3 mb-4">
+                        <i class="bi bi-briefcase"></i>
+                    </div>
+                    <!-- /.icon -->
+                    <div class="service-contents">
+                        <h3>Success Every Day</h3>
+                        <p>
+                            Far far away, behind the word mountains, far from the countries .
+                        </p>
+                    </div>
+                    <!-- /.service-contents-->
+                </a>
+                <!-- /.service -->
+
+                <a href="#" class="service-item link horizontal d-flex" data-aos="fade-left" data-aos-delay="300">
+                    <div class="service-icon color-4 mb-4">
+                        <i class="bi bi-easel"></i>
+                    </div>
+                    <!-- /.icon -->
+                    <div class="service-contents">
+                        <h3>Bring Ideas to Life</h3>
+                        <p>
+                            Far far away, behind the word mountains, far from the countries .
+                        </p>
+                    </div>
+                    <!-- /.service-contents-->
+                </a>
+                <!-- /.service -->
+            </div>
+
+            <div class="col-lg-8">
+                <div class="swiper init-swiper-tabs">
+                    <script type="application/json" class="swiper-config">
+                        {
+                            "loop": true,
+                            "speed": 600,
+                            "autoHeight": true,
+                            "autoplay": {
+                                "delay": 5000
+                            },
+                            "slidesPerView": "auto",
+                            "pagination": {
+                                "el": ".swiper-pagination",
+                                "type": "bullets",
+                                "clickable": true
+                            },
+                            "breakpoints": {
+                                "320": {
+                                    "slidesPerView": 1,
+                                    "spaceBetween": 40
+                                },
+                                "1200": {
+                                    "slidesPerView": 1,
+                                    "spaceBetween": 1
+                                }
+                            }
+                        }
+                    </script>
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/img_h_1.jpg" alt="Image" class="img-fluid">
+                            <div class="p-4">
+                                <h3 class="text-black h5 mb-3">Modern and clean design</h3>
+                                <div class="row">
+                                    <div class="col-lg-8">
+                                        <p>
+                                            Far far away, behind the word mountains, far from the
+                                            countries Vokalia and Consonantia, there live the blind
+                                            texts. Separated they live in Bookmarksgrove right at the
+                                            coast of the Semantics, a large language ocean.
+                                        </p>
+                                        <p>
+                                            A small river named Duden flows by their place and
+                                            supplies it with the necessary regelialia. It is a
+                                            paradisematic country, in which roasted parts of sentences
+                                            fly into your mouth.
+                                        </p>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <ul class="list-unstyled list-check">
+                                            <li>Far far away, behind the word</li>
+                                            <li>Far from the countries Vokalia</li>
+                                            <li>Separated they live in Bookmarksgrove</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/img_h_2.jpg" alt="Image" class="img-fluid">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/img_h_3.jpg" alt="Image" class="img-fluid">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/img_h_4.jpg" alt="Image" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
