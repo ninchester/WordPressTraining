8534721e99f6a7242a616b6fa3d4f223b5885233
ninchester <5041877+ninchester@users.noreply.github.com>
2024-12-05 16:22:26 +0100

    Add separate view for service CPT and enqueue the needed scripts

--- wp-content/themes/softuni/functions.php
+++ wp-content/themes/softuni/functions.php
@@ -2,6 +2,7 @@
 
 add_theme_support('title-tag');
 add_theme_support('post-thumbnails');
+
 add_action('wp_enqueue_scripts', 'softuni_enqueue_assets');
 
 function softuni_enqueue_assets()
@@ -9,6 +10,12 @@ function softuni_enqueue_assets()
     // wp_enqueue_style('softuni-main-style', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), '1.0.0');
     wp_enqueue_style('softuni-main-style', get_stylesheet_uri() . '/assets/css/', array(), '1.1');
 }
+
+function enqueue_bootstrap_icons() {
+    wp_enqueue_style('bootstrap-icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_bootstrap_icons');
+add_action('admin_enqueue_scripts', 'enqueue_bootstrap_icons');
 
 /**
  * This is the main function for registering navigation menus
@@ -20,3 +27,4 @@ function softuni_register_nav_menus()
     register_nav_menu('primary', 'Primary menu');
 }
 add_action('after_setup_theme', 'softuni_register_nav_menus', 0);
+
---
+++ wp-content/themes/softuni/single-service.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+
+<main class="main">
+
+    <!-- Page Title -->
+    <div class="page-title light-background">
+        <div class="container">
+            <h1><?php echo get_the_title(); ?></h1>
+            <nav class="breadcrumbs">
+                <ol>
+                    <li><a href="<?php echo home_url('/'); ?>">Home</a></li>
+                    <li class="current"><?php echo get_the_title(); ?></li>
+                </ol>
+            </nav>
+        </div>
+    </div><!-- End Page Title -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <section id="blog-details" class="blog-details section">
+                    <div class="container">
+                        <article class="article">
+                            <section id="services" class="services section">
+                                <div class="services-item mx-auto">
+                                    <?php
+                                        $icon_class = get_post_meta(get_the_ID(), '_service_icon_class', true);
+                                        echo '<div class="services-icon">';
+                                        if ($icon_class) {
+                                            echo '<i class="' . esc_attr($icon_class) . '"></i>';
+                                        } else {
+                                            echo '<i class="bi bi-question-circle"></i>';
+                                        }
+                                        echo  '</div>'
+                                        ?>
+                                    <h3 class="name"><?php echo get_the_title(); ?></h3>
+                                    <blockquote>
+                                        <p>
+                                            <?php the_content(); ?>
+                                        </p>
+                                    </blockquote>
+                                </div>
+                            </section>
+                        </article>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+</main>
+<?php get_footer(); ?>
