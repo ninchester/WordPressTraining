950f4ccde9fe62da57e8ce05d6382c6b270c6609
ninchester <5041877+ninchester@users.noreply.github.com>
2024-12-06 11:22:31 +0100

    Add 404.php page

---
+++ wp-content/themes/softuni/404.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<main class="main">
+
+    <!-- Page Title -->
+    <div class="page-title light-background">
+        <div class="container">
+            <h1>Page Not Found</h1>
+        </div>
+    </div><!-- End Page Title -->
+
+
+    <section id="blog-posts-2" class="blog-posts-2 section">
+        <div class="container">
+            <div class="row gy-5">
+                <div>Page Not Found</div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
