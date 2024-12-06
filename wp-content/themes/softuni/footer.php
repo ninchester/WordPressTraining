f29c9c444b92fe240a8aa83d8ac5de949d9fa3ec
ninchester <5041877+ninchester@users.noreply.github.com>
2024-12-06 11:55:41 +0100

    Enqueue styles and scripts

--- wp-content/themes/softuni/footer.php
+++ wp-content/themes/softuni/footer.php
@@ -106,18 +106,6 @@
 <!-- Preloader -->
 <div id="preloader"></div>
 
-<!-- Vendor JS Files -->
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/vendor/php-email-form/validate.js"></script>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/vendor/aos/aos.js"></script>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/vendor/swiper/swiper-bundle.min.js"></script>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/vendor/purecounter/purecounter_vanilla.js"></script>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-
-<!-- Main JS File -->
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/main.js"></script>
 <?php wp_footer(); ?>
 </body>
 
--- wp-content/themes/softuni/functions.php
+++ wp-content/themes/softuni/functions.php
@@ -5,13 +5,38 @@ add_theme_support('post-thumbnails');
 
 add_action('wp_enqueue_scripts', 'softuni_enqueue_assets');
 
-function softuni_enqueue_assets()
-{
-    // wp_enqueue_style('softuni-main-style', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), '1.0.0');
-    wp_enqueue_style('softuni-main-style', get_stylesheet_uri() . '/assets/css/', array(), '1.1');
+if (!defined('SOFTUNI_THEME_VER')) {
+    define('SOFTUNI_THEME_VER', '1.0.0');
 }
 
-function enqueue_bootstrap_icons() {
+function softuni_enqueue_assets()
+{
+    // Vendor CSS
+    wp_enqueue_style('bootstrap-min', get_stylesheet_directory_uri() . '/assets/vendor/bootstrap/css/bootstrap.min.css', array(), SOFTUNI_THEME_VER);
+    wp_enqueue_style('bootstrap-icons', get_stylesheet_directory_uri() . '/assets/vendor/bootstrap-icons/bootstrap-icons.css', array(), SOFTUNI_THEME_VER);
+    wp_enqueue_style('aos', get_stylesheet_directory_uri() . '/assets/vendor/aos/aos.css', array(), SOFTUNI_THEME_VER);
+    wp_enqueue_style('swiper-bundle-min', get_stylesheet_directory_uri() . '/assets/vendor/swiper/swiper-bundle.min.css', array(), SOFTUNI_THEME_VER);
+    wp_enqueue_style('glightbox-min', get_stylesheet_directory_uri() . '/assets/vendor/glightbox/css/glightbox.min.css', array(), SOFTUNI_THEME_VER);
+
+    // Theme CSS
+    wp_enqueue_style('softuni-main-style', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), SOFTUNI_THEME_VER);
+
+    // Vendor JS Files
+    wp_enqueue_script('bootstrap-bundle-min', get_stylesheet_directory_uri() . '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', array('jquery'), SOFTUNI_THEME_VER, array('in_footer' => true));
+    wp_enqueue_script('php-email-form-validate', get_stylesheet_directory_uri() . '/assets/vendor/php-email-form/validate.js', array('jquery'), SOFTUNI_THEME_VER, array('in_footer' => true));
+    wp_enqueue_script('aos', get_stylesheet_directory_uri() . '/assets/vendor/aos/aos.js', array('jquery'), SOFTUNI_THEME_VER, array('in_footer' => true));
+    wp_enqueue_script('swiper-bundle-min', get_stylesheet_directory_uri() . '/assets/vendor/swiper/swiper-bundle.min.js', array('jquery'), SOFTUNI_THEME_VER, array('in_footer' => true));
+    wp_enqueue_script('purecounter_vanilla', get_stylesheet_directory_uri() . '/assets/vendor/purecounter/purecounter_vanilla.js', array('jquery'), SOFTUNI_THEME_VER, array('in_footer' => true));
+    wp_enqueue_script('glightbox-min', get_stylesheet_directory_uri() . '/assets/vendor/glightbox/js/glightbox.min.js', array('jquery'), SOFTUNI_THEME_VER, array('in_footer' => true));
+    wp_enqueue_script('imagesloaded-pkgd-min', get_stylesheet_directory_uri() . '/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js', array('jquery'), SOFTUNI_THEME_VER, array('in_footer' => true));
+    wp_enqueue_script('isotope-pkgd-min', get_stylesheet_directory_uri() . '/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js', array('jquery'), SOFTUNI_THEME_VER, array('in_footer' => true));
+
+    // Main JS File
+    wp_enqueue_script('main-js', get_stylesheet_directory_uri() . '/assets/js/main.js', array('jquery'), SOFTUNI_THEME_VER, array('in_footer' => true));
+}
+
+function enqueue_bootstrap_icons()
+{
     wp_enqueue_style('bootstrap-icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css');
 }
 add_action('wp_enqueue_scripts', 'enqueue_bootstrap_icons');
@@ -27,4 +52,3 @@ function softuni_register_nav_menus()
     register_nav_menu('primary', 'Primary menu');
 }
 add_action('after_setup_theme', 'softuni_register_nav_menus', 0);
-
