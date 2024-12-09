a6bc4895c8e5d0ee625a61b48560a553716e2ce9
ninchester <5041877+ninchester@users.noreply.github.com>
2024-12-09 17:09:50 +0100

    Make services-section.php dynamic

--- wp-content/themes/softuni/partials/services-section.php
+++ wp-content/themes/softuni/partials/services-section.php
@@ -1,44 +1,36 @@
-<section id="services" class="services section light-background">
+<?php
 
-    <div class="container">
-        <div class="row gy-4 justify-content-center">
+$query = new WP_Query([
+    'post_type'      => 'service',
+    'posts_per_page' => -1,
+]);
 
-            <div class="col-lg-3">
-                <div class="services-item" data-aos="fade-up">
-                    <div class="services-icon">
-                        <i class="bi bi-bullseye"></i>
+?>
+<?php if ($query->have_posts()) : ?>
+    <section id="services" class="services section light-background">
+        <div class="container">
+            <div class="row gy-4 justify-content-center">
+                <?php while ($query->have_posts()) : $query->the_post(); ?>
+                    <div class="col-lg-3">
+                        <div class="services-item" data-aos="fade-up">
+                            <div class="services-icon">
+                                <?php
+                                $icon_class = get_post_meta(get_the_ID(), '_service_icon_class', true);
+                                if ($icon_class) {
+                                    echo '<i class="' . esc_attr($icon_class) . '"></i>';
+                                } else {
+                                    echo '<i class="bi bi-question-circle"></i>';
+                                }
+                                ?>
+                            </div>
+                            <div>
+                                <h3><?php echo get_the_title(); ?></h3>
+                                <p><?php the_content(); ?></p>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <h3>Technology</h3>
-                        <p>Separated they live in Bookmarksgrove right at the coast</p>
-                    </div>
-                </div>
+                <?php endwhile; ?>
             </div>
-
-            <div class="col-lg-3">
-                <div class="services-item" data-aos="fade-up" data-aos-delay="100">
-                    <div class="services-icon">
-                        <i class="bi bi-command"></i>
-                    </div>
-                    <div>
-                        <h3>Web Design</h3>
-                        <p>Separated they live in Bookmarksgrove right at the coast</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-3">
-                <div class="services-item" data-aos="fade-up" data-aos-delay="200">
-                    <div class="services-icon">
-                        <i class="bi bi-bar-chart"></i>
-                    </div>
-                    <div>
-                        <h3>Branding</h3>
-                        <p>Separated they live in Bookmarksgrove right at the coast</p>
-                    </div>
-                </div>
-            </div>
-
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
